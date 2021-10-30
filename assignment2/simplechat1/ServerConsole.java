cc6ce88dbe69b81a2860a13b22c67a062c6b86c4
philthethrill <pande021@uottawa.ca>
2021-10-30 16:27:36 -0400

    E50c all cases except #quit

--- assignment2/simplechat1/ServerConsole.java
+++ assignment2/simplechat1/ServerConsole.java
@@ -1,3 +1,4 @@
+//this class was created for question 50
 import java.io.*;
 
 import common.ChatIF;
@@ -13,13 +14,87 @@ public class ServerConsole implements ChatIF{
 	
 	public ServerConsole(int port, EchoServer server)
 	{
-		//super(port);
 		this.port = port;	
 		this.server = server;
 	}
 
 	@Override
 	public void display(String message) {
+		if (message.charAt(0) == '#')
+		{
+			String command = message;
+			String[] split = message.split(" ");
+			
+			switch (split[0])
+			{
+			case "#quit":
+				//quit gracefully
+				
+			case "#stop":
+				//only stop listening for new clients, but current clients remain connected
+				if (server.isListening())
+				{
+					server.stopListening();
+					System.out.println("Server stopped listening for connections");
+				}
+				else
+				{
+					System.out.println("Server is not currently listening for connections");
+				}
+				break;
+			case "#close":
+				//stop listening for new connections and disconnect all clients
+				if (server.isListening())
+				{
+					server.stopListening();
+					System.out.println("Server stopped listening for connections. Now disconnecting clients");
+					try {
+						server.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+						System.out.println("Unexpected Error while attempting to close");
+					}
+				}
+				break;
+			case "#setport":
+				//set a port only if server is currently running
+				if (server.isListening())
+				{
+					System.out.println("The Server is already listening for connections");
+				}
+				else
+				{
+					this.server.setPort(Integer.parseInt(split[1]));
+				}
+				break;
+			case "#start":
+				if (server.isListening())
+				{
+					System.out.println("The Server is already listening for connections");
+				}
+				else
+				{
+					try {
+						server.listen();
+					} catch (IOException e) {
+						e.printStackTrace();
+						System.out.println("Unexpected Error, server can't listen for clients");
+					}
+				}
+				break;
+			case "#getport":
+				if (server.isListening())
+				{
+					System.out.println("The Server is listening for clients on port "+server.getPort());
+				}
+				else
+				{
+					System.out.println("The Server is currently not listening for connections");
+				}
+				break;
+			}
+			
+		}
 		String msg = "SERVER MSG>"+message;
 		System.out.println(msg);
 		this.server.sendToAllClients(msg);
